03afa1a7a3c559f2d46f1ebca01111d089b2dcd8
Abhijay Sahay <abhijaygame@gmail.com>
2019-09-09 01:45:05 +0530

    Initial Code

---
+++ morse-converter.hs
@@ -0,0 +1,43 @@
+import System.Environment
+import qualified Data.Map as Map
+
+morseToAlphaMap :: Map.Map String Char
+morseToAlphaMap = Map.fromList $ zip [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."] ['a'..'z']
+alphaToMorseMap :: Map.Map Char String
+alphaToMorseMap = Map.fromList $ zip ['a'..'z'] [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."]
+
+alphaToMorse :: String -> String
+alphaToMorse sentence = unlines $ map wordToCode $ words sentence
+    where
+        wordToCode word =
+            unwords $ map (\c -> 
+                    case Map.lookup c alphaToMorseMap of
+                        Just code -> code
+                        Nothing -> error "Only alphabets and spaces."
+                )
+                word
+
+morseToAlpha :: String -> String
+morseToAlpha code = unwords $ map codeToWord $ lines code
+    where
+        codeToWord code =
+            map (\letterCode ->
+                    case Map.lookup letterCode morseToAlphaMap of
+                        Just letter -> letter
+                        Nothing -> error "Incorrect Code."
+                ) 
+                $ words code
+
+main = do
+    args <- getArgs
+    let
+        output =
+            if length args == 2
+                then let (mode:str:[]) = args
+                    in
+                        case mode of
+                            "-a" -> alphaToMorse str
+                            "-m" -> morseToAlpha str
+                            _ -> error "Allowed modes : -a (alpha to morse), -m (morse to alpha)"
+                else error "Expected two arguments."
+    putStrLn output
